0423430f4122d6e82a57957d7124a3a9a0558fab
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-08-13 00:06:55 +0600

    Footer Cusmized Ready

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <footer className="bg-white dark:bg-gray-900">
+      <footer className="bg-white dark:bg-gray-900 flex justify-center">
         <div className="mx-auto w-full max-w-screen-xl">
           <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-4">
             <div>
@@ -12,22 +12,22 @@ const Footer = () => {
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#!" className=" hover:underline">
+                  <a href="/" className=" hover:underline">
                     About
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/" className="hover:underline">
                     Careers
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/" className="hover:underline">
                     Brand Center
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/" className="hover:underline">
                     Blog
                   </a>
                 </li>
@@ -39,22 +39,42 @@ const Footer = () => {
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a
+                    href="https://discordapp.com/users/108888888888888888"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     Discord Server
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a
+                    href="https://www.twitter.com/MejanurMezan"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     Twitter
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a
+                    href="https://m.me/mejanur.mezan.3"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     Facebook
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a
+                    href="mailto:mrmezan06@gmail.com"
+                    className="hover:underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     Contact Us
                   </a>
                 </li>
@@ -66,17 +86,17 @@ const Footer = () => {
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/privacy-policy" className="hover:underline">
                     Privacy Policy
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/license" className="hover:underline">
                     Licensing
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
+                  <a href="/terms-conditions" className="hover:underline">
                     Terms &amp; Conditions
                   </a>
                 </li>
@@ -84,27 +104,43 @@ const Footer = () => {
             </div>
             <div>
               <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                Download
+                Contact Us
               </h2>
               <ul className="text-gray-500 dark:text-gray-400 font-medium">
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
-                    iOS
+                  <a
+                    href="mailto:mrmezan06@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Email
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
-                    Android
+                  <a
+                    href="https://m.me/mejanur.mezan.3"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Facebook
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
-                    Windows
+                  <a
+                    href="https://wa.me/8801303029292"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    WhatsApp
                   </a>
                 </li>
                 <li className="mb-4">
-                  <a href="#!" className="hover:underline">
-                    MacOS
+                  <a
+                    href="https://t.me/mrmezan06"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Telegram
                   </a>
                 </li>
               </ul>
@@ -112,12 +148,15 @@ const Footer = () => {
           </div>
           <div className="px-4 py-6 bg-gray-100 dark:bg-gray-700 md:flex md:items-center md:justify-between">
             <span className="text-sm text-gray-500 dark:text-gray-300 sm:text-center">
-              © 2023 <a href="https://flowbite.com/">Flowbite™</a>. All Rights
+              © {new Date().getFullYear()}{' '}
+              <a href="https://novel-anime.netlify.app">Novel</a>. All Rights
               Reserved.
             </span>
             <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
               <a
-                href="#!"
+                href="https://www.facebook.com/mejanur.mezan.3"
+                target="_blank"
+                rel="noreferrer"
                 className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg
@@ -136,7 +175,9 @@ const Footer = () => {
                 <span className="sr-only">Facebook page</span>
               </a>
               <a
-                href="#!"
+                href="https://discordapp.com/users/108888888888888888"
+                target="_blank"
+                rel="noreferrer"
                 className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg
@@ -151,7 +192,9 @@ const Footer = () => {
                 <span className="sr-only">Discord community</span>
               </a>
               <a
-                href="#!"
+                href="https://www.twitter.com/MejanurMezan"
+                target="_blank"
+                rel="noreferrer"
                 className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg
@@ -170,7 +213,9 @@ const Footer = () => {
                 <span className="sr-only">Twitter page</span>
               </a>
               <a
-                href="#!"
+                href="https://www.github.com/mrmezan06"
+                target="_blank"
+                rel="noreferrer"
                 className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg
@@ -187,25 +232,6 @@ const Footer = () => {
                   />
                 </svg>
                 <span className="sr-only">GitHub account</span>
-              </a>
-              <a
-                href="#!"
-                className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Dribbble account</span>
               </a>
             </div>
           </div>
