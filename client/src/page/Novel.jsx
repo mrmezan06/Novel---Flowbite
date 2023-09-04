95bf4778da64fa500425279373213a274c0aae39
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-09-04 12:43:57 +0600

    Novel page latest chapter table width remove 80%

--- client/src/page/Novel.jsx
+++ client/src/page/Novel.jsx
@@ -112,7 +112,7 @@ const Novel = () => {
               </div>
             </div>
             {/* Show chapter in two column */}
-            <div className="relative overflow-x-auto mb-5 shadow-md sm:rounded-lg w-[80%]">
+            <div className="relative overflow-x-auto mb-5 shadow-md sm:rounded-lg">
               <table className="w-full text-sm text-left text-gray-500   dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
